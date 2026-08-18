29d7a886269d629302212a110b236a8fc3c71d3d
lyraplasma <274751584+lyraplasma@users.noreply.github.com>
2026-08-18 23:10:16 +0800

    chore:3 init ver

--- graveyard/num_find.pl
+++ graveyard/num_find.pl
@@ -1,3 +1,14 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+
+my $int_total = 0;
+
+while (my $buf_line = <STDIN>) {
+    chomp($buf_line);
+    if ($buf_line =~ m/(\d+)/) {
+	$int_total = $int_total + $1;
+	print "\t\tFound $1, total is now $int_total\n";
+    }
+}
+print "\t\tSum: $int_total\n";
